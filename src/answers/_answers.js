afdd24bcd867df892d09bcd8138a3d52256df00c
James da Silva <jamesodas@gmail.com>
2020-05-05 09:53:10 -0300

    feat(answers): adiciona botão de voltar pra tela de questões

--- src/answers/_answers.js
+++ src/answers/_answers.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 import ToReply from './to-reply';
 import Answer from './answer';
@@ -27,6 +30,9 @@ const Answers = (props) => {
 
   return (
     <div className="answers">
+      <Link to={`/`} className="answers__come-back">
+        <span><FontAwesomeIcon icon={faArrowLeft} size="1x" /></span>voltar
+      </Link>
       <p className="answers__content">
         { question.text }
       </p>
